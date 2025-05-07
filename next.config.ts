e96b00b9c0ca1cdcaef85cf0a2d525ed8613c921
racendol <dragon.arkan259@gmail.com>
2025-05-07 17:34:42 +0900

    add simple landing page in next js

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '', // Change this to your repo name
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Test github page</h1>
+    </div>
+  );
+};
+
+export default Home;
